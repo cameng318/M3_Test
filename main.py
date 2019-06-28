37697e4fe5a3c003ee53cfd30b87de868396eb4f
cameng318 <cameng318@gmail.com>
2019-06-28 18:24:17 -0400

    3 commit

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from SPIStage import *
 x = SPIStage(0,0)
 y = SPIStage(0,1)
 
-x.send('<06 1 00001770>\r')
+x.send('<08 00001770>\r')
 print(x.get())
-y.send('<06 1 00001770>\r')
+y.send('<08 00001770>\r')
 print(y.get())
 
 time.sleep(2)
 
-x.send('<06 1 fffff890>\r')
+x.send('<08 fffff890>\r')
 print(x.get())
-y.send('<06 1 fffff890>\r')
+y.send('<08 fffff890>\r')
 print(y.get())
