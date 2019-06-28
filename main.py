9e8c5c500557423724c6bc5a791a8708b2cc1387
cameng318 <cameng318@gmail.com>
2019-06-28 16:17:07 -0400

    3 commit

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('hi')
+import time
+import spidev
+
+bus = 0
+
+device = 0
+
+spi = spidev.SpiDev()
+
+sip.open(bus, device)
+
+spi.max_speed_hz = 2000000
+
+spi.mode = 0
+
+reply = spi.xfer2([0x01])
+
+print(reply)
