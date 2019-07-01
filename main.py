accf0bc3a4c1195bfdadb8a664c1634a1766b27b
cameng318 <cameng318@gmail.com>
2019-07-01 16:07:50 -0400

    commit

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from SPIStage import *
 x = SPIStage(0, 0)
 y = SPIStage(0, 1)
 
-x.move_to_target(-6000)
-y.move_to_target(6000)
-time.sleep(2)
-
 x.move_to_target(0)
-y.move_to_target(0)
+y.move_to_target(12000)
 time.sleep(2)
 
 x.move_to_target(6000)
 y.move_to_target(6000)
 time.sleep(2)
 
-x.move_to_target(6000)
-y.move_to_target(-6000)
+x.move_to_target(12000)
+y.move_to_target(12000)
+time.sleep(2)
+
+x.move_to_target(12000)
+y.move_to_target(0)
 time.sleep(2)
