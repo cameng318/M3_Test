08905d7e99bbb58dae02d839c6f971dc857a2013
cameng318 <cameng318@gmail.com>
2019-07-01 15:58:53 -0400

    commit

--- rand_closed_loop.py
+++ rand_closed_loop.py
@@ -4,7 +4,10 @@ import random
 x = SPIStage(0, 0)
 y = SPIStage(0, 1)
 
-while 1:
-    x.move_to_target(random.randrange(-6000, 6000, 1))
-    y.move_to_target(random.randrange(-6000, 6000, 1))
-    time.sleep(.5)
+home = [4614, 1457]
+
+x.move_to_target(home[0])
+y.move_to_target(home[1])
+# x.move_to_target(random.randrange(-6000, 6000, 1))
+# y.move_to_target(random.randrange(-6000, 6000, 1))
+time.sleep(.5)
