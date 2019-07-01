37dac6c36e798e8288ed356961db9ea61e3b22a0
cameng318 <cameng318@gmail.com>
2019-07-01 16:05:27 -0400

    commit

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ from SPIStage import *
 x = SPIStage(0, 0)
 y = SPIStage(0, 1)
 
-x.send('<87 4>\r')
-print(x.get())
-y.send('<87 4>\r')
-print(y.get())
+x.move_to_target(-6000)
+y.move_to_target(6000)
+time.sleep(2)
+
+x.move_to_target(0)
+y.move_to_target(0)
+time.sleep(2)
+
+x.move_to_target(6000)
+y.move_to_target(6000)
+time.sleep(2)
+
+x.move_to_target(6000)
+y.move_to_target(-6000)
+time.sleep(2)
