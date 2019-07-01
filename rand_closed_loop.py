9b4f268730649612cc4c124e13969e738ecaca61
cameng318 <cameng318@gmail.com>
2019-07-01 16:00:28 -0400

    commit

--- rand_closed_loop.py
+++ rand_closed_loop.py
@@ -8,6 +8,13 @@ home = [4614, 1457]
 
 x.move_to_target(home[0])
 y.move_to_target(home[1])
-# x.move_to_target(random.randrange(-6000, 6000, 1))
-# y.move_to_target(random.randrange(-6000, 6000, 1))
+time.sleep(.5)
+
+for x in range(10):
+    x.move_to_target(random.randrange(-6000, 6000, 1))
+    y.move_to_target(random.randrange(-6000, 6000, 1))
+    time.sleep(.5)
+
+x.move_to_target(home[0])
+y.move_to_target(home[1])
 time.sleep(.5)
