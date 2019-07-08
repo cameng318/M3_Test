474cde5599bc37714df4a1004bea237fdb9ee78e
cameng318 <cameng318@gmail.com>
2019-07-08 17:02:03 -0400

    commit

--- Deadzone.py
+++ Deadzone.py
@@ -1,3 +1,6 @@
 import os
+import glob
 
-os.system("evdev-joystick --evdev /dev/input/by-id/usb-*-event-joystick --deadzone 0 --fuzz 0")
+files = [f for f in glob.glob("/dev/input/by-id/usb-*-event-joystick")]
+for f in files:
+	os.system("evdev-joystick --evdev " + f + " --deadzone 0 --fuzz 0")
