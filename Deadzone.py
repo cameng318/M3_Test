65cd772a8e31b99795041cbebce1aeb397f5b44d
cameng318 <cameng318@gmail.com>
2019-07-08 16:01:32 -0400

    commit

--- Deadzone.py
+++ Deadzone.py
@@ -1,4 +1,3 @@
 import os
 
-os.system("evdev-joystick --evdev /dev/input/by-id/usb-*-event-joystick --deadzone 0")
-os.system("evdev-joystick --evdev /dev/input/by-id/usb-*-event-joystick --fuzz 0")
+os.system("evdev-joystick --evdev /dev/input/by-id/usb-*-event-joystick --deadzone 0 --fuzz 0")
