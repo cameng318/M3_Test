cb4336f864a59bb06784eb012470b5bbcc9a51d6
cameng318 <cameng318@gmail.com>
2019-07-10 12:50:14 -0400

    commit

--- delay_closed_loop.py
+++ delay_closed_loop.py
@@ -1,4 +1,5 @@
 from SPIStage import *
+from datetime import datetime, date
 
 x = SPIStage(0, 0)
 y = SPIStage(0, 1)
@@ -12,8 +13,14 @@ time.sleep(1)
 x.move_to_target(target[0])
 y.move_to_target(target[1])
 
+start_time = time.time()
+
 for i in range(100):
     time.sleep(.001)
     print(str(i) + ':')
     print(x.view_closed_loop_status_and_position())
     print(y.view_closed_loop_status_and_position())
+
+end_time = time.time()
+duration = datetime.combine(date.today(), start_time) - datetime.combine(date.today(), end_time)
+print(duration)
