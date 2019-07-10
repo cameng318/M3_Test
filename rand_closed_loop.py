851f0d78b9f45a1d4ab880af5de3106728006444
cameng318 <cameng318@gmail.com>
2019-07-10 12:29:30 -0400

    commit

--- rand_closed_loop.py
+++ rand_closed_loop.py
@@ -5,6 +5,7 @@ x = SPIStage(0, 0)
 y = SPIStage(0, 1)
 
 home = [4614, 1457]
+target = [6000,6000]
 
 x.move_to_target(home[0])
 y.move_to_target(home[1])
@@ -15,8 +16,8 @@ print(x.view_closed_loop_status_and_position())
 print(y.view_closed_loop_status_and_position())
 
 for i in range(10):
-    x.move_to_target(random.randrange(-6000, 6000, 1))
-    y.move_to_target(random.randrange(-6000, 6000, 1))
+    target = [random.randrange(0, 12000, 1), random.randrange(0, 12000, 1)]
+    x.move_to_target(target[0])
     time.sleep(1)
     print(str(i) + ':')
     print(x.view_closed_loop_status_and_position())
