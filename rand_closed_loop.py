45132d88677d1857bdbed0ea29ed5d1cdf49965d
cameng318 <cameng318@gmail.com>
2019-07-10 12:34:50 -0400

    commit

--- rand_closed_loop.py
+++ rand_closed_loop.py
@@ -19,6 +19,7 @@ print(y.view_closed_loop_status_and_position())
 for i in range(10):
     target = [random.randrange(0, 12000, 1), random.randrange(0, 12000, 1)]
     x.move_to_target(target[0])
+    y.move_to_target(target[1])
     time.sleep(delay)
     print(str(i) + ':')
     print(x.view_closed_loop_status_and_position())
