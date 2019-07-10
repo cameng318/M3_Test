0fc76ab260a0a46b565ffa9bd06995d3663a7e7e
cameng318 <cameng318@gmail.com>
2019-07-10 13:22:12 -0400

    commit

--- SPIStage.py
+++ SPIStage.py
@@ -19,7 +19,7 @@ class SPIStage:
         time.sleep(.00006)
 
     def get(self):
-        reply = self.axis.readbytes(32)
+        reply = self.axis.readbytes(35)
         message = ''.join([chr(x) for x in reply]).strip('\x01')
         time.sleep(.00006)
         return message
