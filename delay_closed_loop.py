93e9fe7bee1b311e8fb20c3e99e1f4bc2c3ba371
cameng318 <cameng318@gmail.com>
2019-07-10 12:40:13 -0400

    commit

--- delay_closed_loop.py
+++ delay_closed_loop.py
@@ -1,19 +1,18 @@
 from SPIStage import *
-import random
 
 x = SPIStage(0, 0)
 y = SPIStage(0, 1)
 
 home = [4614, 1457]
-target = [4714,1457]
+target = [4714, 1457]
 
 x.move_to_target(home[0])
 y.move_to_target(home[1])
 time.sleep(1)
+x.move_to_target(target[0])
+y.move_to_target(target[1])
 
 for i in range(100):
-    x.move_to_target(target[0])
-    y.move_to_target(target[1])
     time.sleep(.001)
     print(str(i) + ':')
     print(x.view_closed_loop_status_and_position())
