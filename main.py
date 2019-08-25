db4083f352be814d36644c1452dc7bebea4a122b
cameng318 <cameng318@gmail.com>
2019-08-25 15:53:04 -0400

    commit

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ while True:
         if (abs(axes[0]) < 0.05) and (abs(axes[1]) < 0.05):
             Homed = True
 
-    print(position)
+    print(position, sensitivity_level)
     x.move_to_target(position[0])
     y.move_to_target(position[1])
 
