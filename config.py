3d0aa076bf6e244c4a95ba30dc721af047224f48
cameng318 <cameng318@gmail.com>
2019-09-25 19:30:53 -0400

    commit

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ AbsMin = 0
 Sensitivity = [6000, 3000, 1500, 750, 375, 188, 94, 47, 23, 12]
 
 # Sensitivity for z axis (step per second)
-Z_Sensitivity_High = 10
-Z_Sensitivity_Low = 3
+Z_Sensitivity_High = 100
+Z_Sensitivity_Low = 30
 
 # Threshold level for homing an axis
 Home_threshold = 0.01
